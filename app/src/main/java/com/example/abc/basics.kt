2b8bc7d82da5fe808758ae482bc288f702dcc6bc
salifa11 <salifashrestha5000@gmail.com>
2025-11-03 19:55:37 +0545

    Initial commit

---
+++ app/src/main/java/com/example/abc/basics.kt
@@ -0,0 +1,16 @@
+package com.example.abc
+
+fun main() {
+    // Immutable variable (cannot be changed)
+    val name: String = "Alice"
+
+    // Mutable variable (can be changed)
+    var age: Int = 25
+
+    println("Name: $name")
+    println("Age: $age")
+
+    // Changing the value of 'age'
+    age = 26
+    println("Updated Age: $age")
+}
